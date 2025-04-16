defafcd89a363a1897cb1c6697f72e0426530842
jubisse <jubisse@hotmail.com>
2025-04-16 08:40:13 +0200

    first commit

---
+++ a1.js
@@ -0,0 +1,14 @@
+/*1. Crie uma função nomeada que receba um array de números e retorne a soma
+de todos os elementos.*/
+
+const nomeada = [1, 2, 3, 4, 5];
+function nomeada() {
+    let add = 0;
+    for (let i = 0; i < array.length; i++) {
+        add = array[i] + array[i];
+
+    }
+    return add;
+
+}
+console.log(nomeada());
