9ad63a44b53dacf3963f1bed282699d4fcccd5b1
jubisse <jubisse@hotmail.com>
2025-04-17 09:42:14 +0200

    have problem

---
+++ c2.js
@@ -0,0 +1,11 @@
+/**
+ * 2. Filtre os números ímpares de um array usando .filter(). 
+ */
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const filtrar = array.filter(array);
+function filtrara(array) {
+    return array % 2 === 1;
+}
+console.log(filtrara);
