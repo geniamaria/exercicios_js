481ba9dd5277353195e51771980a073264543154
jubisse <jubisse@hotmail.com>
2025-04-17 09:49:38 +0200

    find especifice number with method .find()

---
+++ c5.js
@@ -0,0 +1,7 @@
+
+/**
+ * Use .find() para encontrar um número específico dentro de um array. 
+ */
+const array = [1, 10, 3, 8, 5, 0, 7, 4, 9, 2];
+const findNumber = array.find(num => num > 15);
+console.log(findNumber);
