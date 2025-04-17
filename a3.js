b936031a97bd0a717d96bd9c6e95a000119f3d76
jubisse <jubisse@hotmail.com>
2025-04-17 08:10:16 +0200

    resolve probleme a3

---
+++ a3.js
@@ -0,0 +1,17 @@
+/*
+       3.  Escreva uma função anônima que receba um array de strings e retorne a
+        string com mais caracteres.*/
+
+const array = ["Maria", "Genia", "Abilio", "Moiane"];
+let longestString = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > longest.length) {
+            return longest = array[i];
+        }
+    }
+
+}
+console.log(longestString(array));
+
+
+
