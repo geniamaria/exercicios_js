2866f5a4067cb4ab2bc44f852b318122fc081f43
jubisse <jubisse@hotmail.com>
2025-04-17 09:26:34 +0200

    list object.keys

---
+++ b4.js
@@ -0,0 +1,10 @@
+//Use Object.keys() para listar todas as chaves de um objeto qualquer.
+
+let obj = {
+    marca: "Mazda",
+    modelo: "Demio",
+    ano: 2019
+};
+
+let keys = Object.keys(obj);
+console.log(keys);
