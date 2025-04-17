09793968f6ebc5fcc3393457a12c0a679017856f
jubisse <jubisse@hotmail.com>
2025-04-17 12:50:26 +0200

    merge both object

--- b9.js
+++ b9.js
@@ -2,8 +2,3 @@
  *  Utilize Object.entries() para iterar sobre as chaves e valores de um objeto e exibir 
 no console. 
  */
-
-let obj1 = { name: "Maria", surname: "Moiane" };
-let obj2 = { name: "Maria", surname: "Moiane" };
-
-obj1.merge(obj2);
