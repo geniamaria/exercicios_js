1e730b4139caad62da4ca1bdc896a6ca8eaf9c23
jubisse <jubisse@hotmail.com>
2025-04-17 09:28:10 +0200

    list object.values

---
+++ b5.js
@@ -0,0 +1,10 @@
+//Use Object.values() para listar todos os valores de um objeto qualquer.
+
+
+let obj = {
+    cor: "verde",
+    kilometragem: "1000km",
+    ano: 2024
+};
+let value = Object.values(obj);
+console.log(value);
