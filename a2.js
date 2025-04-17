c43b6e9ca6b8cc729affca62aa18cf1507151cba
jubisse <jubisse@hotmail.com>
2025-04-17 08:07:00 +0200

    create file a2.js and resovel the exercise

---
+++ a2.js
@@ -0,0 +1,12 @@
+/*2. Crie uma arrow function que receba um número e retorne seu fatorial.*/
+
+
+let factorial = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+
+    } else {
+
+    } return nr * factorial(nr - 1);
+}
+console.log(factorial(5));
