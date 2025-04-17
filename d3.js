be3ede4cc510ef763b5fe59c57e9a2c17a181b5e
jubisse <jubisse@hotmail.com>
2025-04-17 10:21:49 +0200

    arround use  Math.seil, math.floor, math.round

---
+++ d3.js
@@ -0,0 +1,10 @@
+/**
+ * Arredonde um número para cima, para baixo e para o inteiro mais próximo 
+usando Math.ceil(), Math.floor() e Math.round(). 
+ */
+
+const n = 5.2;
+console.log(Math.random(n));
+console.log(Math.floor(n));
+console.log(Math.ceil(n));
+console.log(Math.trunc(n));
