c626c545244f5e854c770df1ff98f192b039592b
jubisse <jubisse@hotmail.com>
2025-04-17 08:12:51 +0200

    troubleshooting a4

---
+++ a4.js
@@ -0,0 +1,12 @@
+/*4.  Crie uma função que receba um número e retorna true se for primo e false   caso contrário.*/
+
+const n = 20;
+let boolean = (n) => {
+    if (n % 2 === 1) {
+        return false;
+    } else {
+        return false;
+    }
+
+}
+console.log(boolean());
