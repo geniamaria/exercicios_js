e150fd6184662bd93e0fe07e782f3d26570a2e30
jubisse <jubisse@hotmail.com>
2025-04-17 09:21:13 +0200

    i changed the resolution of the exercises the resolution of b2 is in b3

---
+++ b3.js
@@ -0,0 +1,11 @@
+//Adicione um método ao objeto carro que retorne uma string com suas informações.
+
+const car = {
+    marca: 'Toyota',
+    modelo: 'Ratis',
+    ano: 2007
+
+};
+
+car.s
+console.log(car);
