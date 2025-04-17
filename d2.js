38ce201ece3ad1c75b1e2a24d5b20eb5ed7fc386
jubisse <jubisse@hotmail.com>
2025-04-17 10:16:53 +0200

    find  longest number use Math.max and math.min

---
+++ d2.js
@@ -0,0 +1,7 @@
+/**
+ * Encontre o maior e o menor número dentro de um array usando Math.max() e 
+Math.min(). 
+ */
+const array = [1, 2, 3, 4];
+console.log(Math.max(array[1, 2, 3, 4]));
+onsole.log(Math.min(array[1, 2, 3, 4]));
