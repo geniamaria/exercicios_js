cfeacfbe975664a4747e7f63eada4545765c153c
jubisse <jubisse@hotmail.com>
2025-04-17 10:02:12 +0200

    use forEach() to show array comsole

---
+++ c8.js
@@ -0,0 +1,10 @@
+/**
+ * . Utilize .forEach() para imprimir cada item de um array no console. 
+ *
+ */
+const array = [4, 5, 6, 7, 8,];
+array.forEache((item, index) => {
+    console.log(item + 'is ate index' + index)
+
+});
+
